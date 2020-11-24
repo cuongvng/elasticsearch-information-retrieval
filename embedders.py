20e776e2b63c0fa326a2e695a4b21d2be6fac3a6
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-24 09:22:20 +0700

    add bert

--- embedders.py
+++ embedders.py
@@ -28,3 +28,29 @@ class SwivelEmbedder(EmbedderBase):
     def embed_list_text(self, list_text):
         return np.array(self.embedder(tf.constant(list_text)))
 
+class BertEmbedder(EmbedderBase):
+    def __init__(self, bert_link="https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-128_A-2/1",
+                 preprocessor_link="https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/1",
+                 sequence_length=128):
+        self.preprocessor = hub.load(preprocessor_link)
+        self.embedder = hub.KerasLayer(bert_link, trainable=False)
+        self.sequence_length = sequence_length
+        print(f"Loaded pre-trained model {bert_link} successfully!")
+
+    def embed_single_text(self, plain_text):
+        text = tf.constant([plain_text])
+        tokenized_text = self.preprocessor.tokenize(text)
+        encoder_input = self.preprocessor.bert_pack_inputs(
+            [tokenized_text], seq_length=self.sequence_length) # ['input_mask', 'input_type_ids', 'input_word_ids']
+
+        outputs = self.embedder(encoder_input)
+        return np.array(outputs["pooled_output"]) # Embedding of the whole sentence, shape [1, 768]
+
+    def embed_list_text(self, list_text):
+        text = tf.constant(list_text)
+        tokenized_text = self.preprocessor.tokenize(text)
+        encoder_input = self.preprocessor.bert_pack_inputs(
+            [tokenized_text], seq_length=self.sequence_length)  # ['input_mask', 'input_type_ids', 'input_word_ids']
+
+        outputs = self.embedder(encoder_input)
+        return np.array(outputs["pooled_output"])  # Embedding of the whole sentence, shape [1, 768]
