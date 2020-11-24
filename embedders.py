9645eb633ca62edc81873ec930bd41518199570d
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-11-24 09:21:59 +0700

    add swivel

--- embedders.py
+++ embedders.py
@@ -15,3 +15,16 @@ class EmbedderBase(object):
 
     def embed_list_text(self, list_text):
         pass
+
+class SwivelEmbedder(EmbedderBase):
+    def __init__(self, model_link="https://tfhub.dev/tensorflow/cord-19/swivel-128d/3"):
+        self.embedder = hub.KerasLayer(model_link)
+        print(f"Loaded pre-trained model {model_link} successfully!")
+
+    def embed_single_text(self, plain_text):
+        formated_text = tf.constant([plain_text])
+        return np.array(self.embedder(formated_text))
+
+    def embed_list_text(self, list_text):
+        return np.array(self.embedder(tf.constant(list_text)))
+
